758ef3124bfc350496e759e2ba4c569001a76458
junior-ramos-dev <junior.ramos.dev@gmail.com>
2024-10-14 18:36:05 -0300

    Refactored handleResponse;

--- src/core/loaders/taskHandlerWrapper.ts
+++ src/core/loaders/taskHandlerWrapper.ts
@@ -38,27 +38,13 @@ export const handleResponse = (result: ITask | undefined, res: Response) => {
   // Check if the result is defined
   if (result) {
     // Destructure the result for convenient access
-    const { data, error, taskId } = result;
-
-    // Handle the scenario where the taskId is defined but no data is available
-    if (!data && taskId) {
-      // Log the condition and send a successful response with the result object
-      // console.log(
-      //   `Task ${taskId} executed successfully, but no data was returned.`
-      // );
-      return res.status(200).json(result);
-    }
+    const { error } = result;
 
     // Handle the scenario where there is an error present in the response
     if (error && error.status > 0) {
-      // console.error(`Error occurred while processing task: ${error.message}`);
-      // Send response status as per the error object
       return res.status(error.status).json(error);
-    }
-
-    // Handle case when data is present
-    if (data) {
-      // console.log(`Task ${taskId} executed successfully.`);
+    } else {
+      // Handle the scenario where there is no error present in the response
       return res.status(200).json(result);
     }
   }
