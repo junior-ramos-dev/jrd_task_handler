9965eb08a72d47e1b22b02cd58be70b88945bab1
junior-ramos-dev <junior.ramos.dev@gmail.com>
2024-10-14 17:08:51 -0300

    Minor changes;

--- src/core/loaders/taskHandlerWrapper.ts
+++ src/core/loaders/taskHandlerWrapper.ts
@@ -43,22 +43,22 @@ export const handleResponse = (result: ITask | undefined, res: Response) => {
     // Handle the scenario where the taskId is defined but no data is available
     if (!data && taskId) {
       // Log the condition and send a successful response with the result object
-      console.log(
-        `Task ${taskId} executed successfully, but no data was returned.`
-      );
+      // console.log(
+      //   `Task ${taskId} executed successfully, but no data was returned.`
+      // );
       return res.status(200).json(result);
     }
 
     // Handle the scenario where there is an error present in the response
     if (error && error.status > 0) {
-      console.error(`Error occurred while processing task: ${error.message}`);
+      // console.error(`Error occurred while processing task: ${error.message}`);
       // Send response status as per the error object
       return res.status(error.status).json(error);
     }
 
     // Handle case when data is present
     if (data) {
-      console.log(`Task ${taskId} executed successfully.`);
+      // console.log(`Task ${taskId} executed successfully.`);
       return res.status(200).json(result);
     }
   }
